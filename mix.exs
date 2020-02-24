ce510fc47e2e77071011387d1aeb0bf378dfdd45
takasehideki <takase@i.kyoto-u.ac.jp>
2020-02-24 18:33:16 +0900

    add rclex package from GitHub (for development)

--- mix.exs
+++ mix.exs
@@ -21,6 +21,11 @@ defmodule RclexSamples.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # NOTE: we specify GitHub link since Rclex is now under development
+      # {:rclex, "~> 0.0.1"},
+      {:rclex, git: "https://github.com/tlk-emb/rclex", branch: "refactoring"}
+      # {:rclex, path: "../rclex"}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
