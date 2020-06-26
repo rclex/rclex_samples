0350c265b61fe78301359e003f69a7977cde50ac
takasehideki <takase@i.kyoto-u.ac.jp>
2020-06-26 22:03:33 +0900

    add demo on rosjp 37

---
+++ lib/rclex_samples/simple_pub_rosjp.ex
@@ -0,0 +1,31 @@
+defmodule RclexSamples.SimplePub do
+  @moduledoc """
+    The sample which makes any number of publishers.
+  """
+  def pub_main(num_node) do
+    Rclex.rclexinit()
+    |> Rclex.create_nodes('test_pub_node', num_node)
+    |> Rclex.create_publishers('testtopic', :single)
+    |> Rclex.Timer.timer_start(1000, &callback/1)
+
+  end
+
+  @doc """
+    Timer event callback function defined by user.
+  """
+  def callback(publisher_list) do
+    # Create messages according to the number of publishers.
+    n = length(publisher_list)
+    msg_list = Rclex.initialize_msgs(n, :string)
+    data = "hello,world"
+    IO.puts("publish message:#{data}")
+    # Set data.
+    Enum.map(0..(n - 1), fn index ->
+      Rclex.setdata(Enum.at(msg_list, index), data, :string)
+    end)
+
+    # Publish topics.
+    # IO.puts("pub time:#{:os.system_time(:microsecond)}")
+    Rclex.Publisher.publish(publisher_list, msg_list)
+  end
+end
